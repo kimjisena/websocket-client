a7601f649b82ca7146b5c5f0c9dc87bf7136a8ac
Kim Jisena <pjohmaths@gmail.com>
2023-01-22 16:57:15 +0300

    implement a simple WebSocket client

---
+++ index.js
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv';
+import WebSocket from 'ws';
+
+dotenv.config();
+
+const sessionOpts = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: encodeURI(`email=${process.env.EMAIL}&password=${process.env.PASSWORD}`),
+};
+
+fetch(process.env.HTTP_SERVER_URL, sessionOpts)
+    .then((res) => {
+        if (res.ok) {
+            let cookie = res.headers.get('set-cookie').split(';')[0];
+            const opts = {
+                headers: {
+                    'Cookie': cookie,
+                }
+            };
+            const ws = new WebSocket(process.env.SOCKET_SERVER_URL, opts);
+            
+            ws.on('open', function open() {
+                ws.send('something');
+            });
+
+            ws.on('message', function message(data) {
+                let parsedData = JSON.parse(data);
+                console.log('received: %o', parsedData);
+            });
+
+            ws.on('unexpected-response', function unexpected(req, res) {
+                console.log('Client Request: %o', req);
+            });
+
+            ws.on('error', function error (data) {
+                console.log('we have an error: %s', data.message);
+            });
+        } else {
+            console.log(res.status, res.statusText);
+        }
+});
+
